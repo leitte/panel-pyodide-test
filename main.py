8773a92ccf8f31eb22ae37d69a3b41d3ca40e690
Heike Leitte <65338290+leitte@users.noreply.github.com>
2025-11-11 13:54:28 +0100

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ slider = pn.widgets.FloatSlider(start=0, end=10, name="Amplitude")
 def cb(v):
     return f"Amplitude is: {v}"
 
-pn.Row(slider, pn.bind(cb, slider)).servable(target="app")
+#pn.Row(slider, pn.bind(cb, slider)).servable(target="app")
 # or: await pn.io.pyodide.write('app', pn.Row(...))  if you prefer explicit write
 
 
