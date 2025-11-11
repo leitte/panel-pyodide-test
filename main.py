e94de050b2e0a4d3f7253f5438fdc1db730ad4c4
Heike Leitte <65338290+leitte@users.noreply.github.com>
2025-11-11 13:57:34 +0100

    add contents

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ slider = pn.widgets.FloatSlider(start=0, end=10, name="Amplitude")
 def cb(v):
     return f"Amplitude is: {v}"
 
-#pn.Row(slider, pn.bind(cb, slider)).servable(target="app")
+slider = pn.Row(slider, pn.bind(cb, slider)).servable(target="app")
 # or: await pn.io.pyodide.write('app', pn.Row(...))  if you prefer explicit write
 
 
 app = pn.template.FastListTemplate(
     site="Forecast Lab",
     title="Anomaly Explorer",
-    theme="default", 
+    theme="default",
+    main=[slider]
 )
 
 await pn.io.pyodide.write('app', app)
