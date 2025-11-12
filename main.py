39383e8fb40de7d55e4bfe713748d7a07e7f3c12
Heike Leitte <leitte@rptu.de>
2025-11-12 15:13:51 +0100

    update

--- main.py
+++ main.py
@@ -3,12 +3,34 @@ import io
 import panel as pn
 import pandas as pd
 import numpy as np
-from panel.template import FastListTemplate
 import holoviews as hv
 
 # Make sure Panel extensions loaded
 pn.extension(sizing_mode="stretch_width")
 hv.extension('bokeh')
+
+hv.opts.defaults(hv.opts.Curve(color=hv.Cycle(["#145592","#0cad7f","#ffc43a","#EB5B67","#592835","#00b0be"])),
+                 hv.opts.Violin(cmap=["#145592","#0cad7f","#ffc43a","#EB5B67","#592835","#00b0be"])
+                 )
+
+def hook(plot, element):
+    # print('plot.state:   ', plot.state)
+    # print('plot.handles: ', sorted(plot.handles.keys()))
+    plot.handles['xaxis'].major_label_text_color = '#71717A'
+    plot.handles['yaxis'].major_label_text_color = '#71717A'
+    plot.handles['xaxis'].axis_label_text_color = '#71717A'
+    plot.handles['yaxis'].axis_label_text_color = '#71717A'
+    plot.handles['xaxis'].formatter.context = None
+
+    if plot.handles['plot'].legend:
+        plot.handles['plot'].legend.label_text_color = '#333'
+        plot.handles['plot'].legend.label_text_font_size = '9pt'
+        plot.handles['plot'].legend.border_line_color = None
+        plot.handles['plot'].legend.orientation = "horizontal"
+
+def minimal_style_hook(plot, element):
+    plot.handles['plot'].outline_line_color = None
+    plot.handles['yaxis'].minor_tick_line_color = None
 
 # widgets / content
 slider = pn.widgets.FloatSlider(name="Amplitude", start=0, end=10, value=5)
@@ -26,9 +48,20 @@ sidebar = pn.Column(
     pn.panel(text),
 )
 
-main_content = pn.Column(
-    hv_pane
+main_content = pn.Tabs(
+    ('Data', pn.Column(hv_pane))
 )
+
+def time_series_plot(df, features):
+    p = [hv.Curve(df, 'Time', tag, label=tag) for tag in features]
+    overlay = hv.Overlay(p)
+    overlay.opts(hv.opts.Curve(height=250, responsive=True, 
+                               active_tools=[]))
+    overlay.opts(xlabel='', ylabel='', 
+                    show_grid=True, legend_position='top_left', 
+                    hooks=[hook, minimal_style_hook]
+                    )
+    return overlay
 
 def _on_file_change(event):
     #print(event)
@@ -36,13 +69,16 @@ def _on_file_change(event):
     df = pd.read_csv(buffer)
     df['Time'] = pd.to_datetime(df.Time, format='%H:%M:%S')
 
-    p = [hv.Curve(df, 'Time', tag, label=tag) for tag in ['T702', 'T704']]
-    overlay = hv.Overlay(p)
-    overlay.opts(xlabel='', ylabel='', 
-                    show_grid=True, legend_position='top_left', 
-                    #hooks=[hook]
-                    )
-    hv_pane.object = overlay
+    temp_sensors = ['T703', 'T709', 'T711', 'T712', 'T705']
+    temp_heaters = ['T701', 'T702', 'T704', 'T706', 'T708']
+    pressure_difference = ['PDI701', 'PDI702']
+    pressure_sensor = ['PY23']
+    flow_transport = ['FT703', 'FT704']
+
+    plots = [time_series_plot(df, f) for f in [temp_sensors, temp_heaters, pressure_difference, pressure_sensor, flow_transport]]
+    layout = hv.Layout(plots).cols(1)
+    layout.opts(sizing_mode="stretch_width")
+    hv_pane.object = layout
 
 file_input.param.watch(_on_file_change, "value")
 
